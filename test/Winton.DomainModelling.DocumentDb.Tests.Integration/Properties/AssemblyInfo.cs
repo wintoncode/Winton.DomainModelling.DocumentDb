8a765aff1c65203f4220c63f5bb2a2a69b13f21e
Iain Scott <i.scott@winton.com>
2018-07-06 09:19:55 +0100

    Clean up for open source.

--- test/Winton.DomainModelling.DocumentDb.Tests.Integration/Properties/AssemblyInfo.cs
+++ test/Winton.DomainModelling.DocumentDb.Tests.Integration/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
-﻿using Xunit;
+﻿// Copyright (c) Winton. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Xunit;
 
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
